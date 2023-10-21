05e6565b72afe3081c8b8e0c83e2547fa0e6d0b9
CostasAK <costas@kokke.eu>
2023-10-21 16:13:19 +0200

    test: add vitest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./env",
   plugins: [react(), svgrPlugin()],
+  test: { environment: "happy-dom" },
 });
