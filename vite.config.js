ee60396ae585cf1d9109dc5fe088077ab28d9c48
CostasAK <costas@kokke.eu>
2023-10-22 16:16:57 +0200

    feat: replace imagemin with vite image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
@@ -5,6 +6,6 @@ import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./env",
-  plugins: [react(), svgrPlugin()],
+  plugins: [react(), svgrPlugin(), ViteImageOptimizer({ test: /\.svg$/i })],
   test: { environment: "happy-dom" },
 });
