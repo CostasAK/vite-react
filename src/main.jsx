65f79ac78ca75f610bb92cdb4726da773383d3ae
CostasAK <costas@kokke.eu>
2023-12-20 14:30:16 +0100

    fix: provide a renderwrapper including the helmet provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Helmet } from "react-helmet-async";
+import { RenderWrapper } from "./components/RenderWrapper";
 import { Root } from "./layout/Root";
 
 const rootElement = document.getElementById("root");
 
 createRoot(rootElement).render(
   <StrictMode>
-    <Helmet
-      defaultTitle={import.meta.env.VITE_APP_NAME}
-      titleTemplate={`%s - ${import.meta.env.VITE_APP_NAME}`}
-    />
-    <Root />
+    <RenderWrapper>
+      <Root />
+    </RenderWrapper>
   </StrictMode>,
 );
